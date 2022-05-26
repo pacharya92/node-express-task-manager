be86b6a95359b2823e03fe467a7e7e5e729f95c0
Prakash Acharya <prakashacharya1992@gmail.com>
2022-05-26 03:28:23 -0400

    Create JWT when user logins or creates new account
      - Add Tokens field to the user model
      - The Tokens field are an array of JWT tokens associated with the user
      - Token persists in the database for authentication

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 import validator from 'validator'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -41,10 +42,22 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }],
 })
 
 userSchema.methods.generateAuthToken = async function () {
-  const user = await User.findOne({ email })
+  const user = this
+  const token = jwt.sign({_id: user._id.toString() }, 'thisisanewproject')
+
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+
+  return token 
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
--- src/routers/user.js
+++ src/routers/user.js
@@ -6,9 +6,10 @@ const router = new express.Router()
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body)
+  const token = await user.generateAuthToken()
   try {
     await user.save()
-    res.status(201).send(user)
+    res.status(201).send({ user, token })
   } catch (err) {
     res.status(400).send(err)
   }
@@ -17,7 +18,8 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async(req, res) => {
   try {
     const user =  await User.findByCredentials(req.body.email, req.body.password)
-    res.send(user)
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
   } catch(e){
       res.status(400).send()
   }
